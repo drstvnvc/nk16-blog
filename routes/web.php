dc5e811fdbaf218dfcc36f273ed7e7b83ff77adc
Drago Stevanovic <drago@sounder.fm>
2022-02-09 20:33:54 +0100

    Add two pages. 1 - display all published post. 2 - display a post by id

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/posts', function () {
+    $posts = Post::where('is_published', true)->get();
+
+    return view('posts', compact('posts')); // [ 'posts' => $posts]
+});
+
+Route::get('/posts/{id}', function ($id) {
+    // $post = Post::where('id', $id)->first();
+    $post = Post::findOrFail($id);
+
+    return view('post', compact('post')); // [ 'post' => $post]
 });
